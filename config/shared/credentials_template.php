92d36c1248a2843c4f3412d084137c4b05d8d5ea
Marcelo Garcia <marcelo.garcia@kaust.edu.sa>
2025-10-25 09:02:33 +0000

    Templates polute the find

--- config/shared/credentials_template.php
+++
@@ -1,6 +0,0 @@
-<?php	
-	define('MYSQL_USER', '');
-	define('MYSQL_PW', '');
-	
-	define('REPOSITORY_USER', '');
-	define('REPOSITORY_PW', '');
--- config/shared/settings_template.php
+++
@@ -1,7 +0,0 @@
-<?php
-	// change it to 0 when moving to production, this prevents details of some warnings and errors from being directly displayed to users
-	ini_set('display_errors', 1);
-
-    // keep consistent character encoding settings in scripts and databases
-	ini_set('default_charset', 'UTF-8' );
-	mb_internal_encoding("UTF-8");
